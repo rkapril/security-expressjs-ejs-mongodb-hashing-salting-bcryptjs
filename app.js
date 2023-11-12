2025a8501e3f0676637b20cd0598c91f1d6c27ff
Ray Goh <61505106+rkapril@users.noreply.github.com>
2023-11-12 20:50:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -16,9 +16,7 @@ app.use(
   })
 );
 
-mongoose.connect("mongodb://127.0.0.1:27017/userDB", {
-  useNewUrlParser: true,
-});
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
 const userSchema = new mongoose.Schema({
   email: String,
